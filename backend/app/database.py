9b851bd15a635264fecf0a5569c8a0196ccff9ea
KaiSong06 <kai.song421@gmail.com>
2025-06-20 23:12:35 -0400

    FastAPI init with postgres

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
 
@@ -14,7 +15,8 @@ DBNAME = os.getenv("dbname")
 DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
-
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 #Create branch
 
 # Test the connection
---
+++ backend/app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+from app.database import SessionLocal, Base, engine
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+def test_db(db: Session = Depends(get_db)):
+    result = db.execute(text("SELECT version();"))
+    version = result.fetchone()
+    return {"PostgreSQL Version": version[0]}
